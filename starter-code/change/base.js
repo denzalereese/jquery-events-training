38b68d9876c1da6dfdaaf1a7fe9159e1f968c851
Denzale Reese <dlr249@cornell.edu>
2016-08-18 16:25:55 -0700

    updates the total when 'add' form values are changed

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -3,5 +3,9 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 
   $("#time").text( Date.now() );
+  $(".add").change(function() {
+    var total = parseInt($("#left").val()) +  parseInt($("#right").val());
+    $("#total").val(total);
+  });
 
 })
